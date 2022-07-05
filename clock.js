7e0aa8b1e33055ae58e1473fd3d19108ae541d1b
Chinmayi Khachane <95357417+chinmayi2205@users.noreply.github.com>
2022-07-05 20:50:24 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,54 @@
+// setInterval(() =>{
+//   let hours = document.getElementById('hours');
+//   let minutes = document.getElementById("minutes");
+//   let seconds = document.getElementById("seconds");
+//   let ampm = document.getElementById("ampm");
+
+//   let hh = document.getElementById("hh");
+//   let mm = document.getElementById("mm");
+//   let ss = document.getElementById("ss");
+
+//   let hr_dot = document.getElementById('.hr_dot');
+//   let min_dot = document.getElementById('.min_dot');
+//   let sec_dot = document.getElementById('.sec_dot');
+
+//   let h = new Date().getHours();
+//   let m = new Date().getMinutes();
+//   let s = new Date().getSeconds();
+//   let am = h >= 12 ? "PM" : "AM";
+
+//   if (h > 12){
+//     h = h - 12;
+//   }
+//   h = (h < 10) ? "0" + h : h;
+//   m = (m < 10) ? "0" + m : m;
+//   s = (s < 10) ? "0" + s : s;
+
+//   hours.innerHTML = h +"<br><span>Hours</span>";
+//   minutes.innerHTML = m + "<br><span>Minutes</span>";
+//   seconds.innerHTML = s + "<br><span>Seconds</span>";
+//   ampm.innerHTML = am;
+
+//   //hr_dot.style.transform = `rotate(${h * 30}deg)`;
+
+//   hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+//   mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+//   ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+//   hr_dot.style.transform =`rotate(${h * 30}deg)`;
+//   min_dot.style.transform =`rotate(${m * 6}deg)`;
+//   sec_dot.style.transform =`rotate(${s * 6}deg)`;
+
+// })
+// var today = new Date();
+// const dayNumber = today.getDate();
+// const year = today.getFullYear();
+// const dayName = today.toLocaleString("default", {weekday: "long"});
+// const monthName = today.toLocaleString("default", {month: "short"});
+
+// document.querySelector(".month").innerHTML = monthName;
+// document.querySelector(".dayname").innerHTML = dayName;
+// document.querySelector(".daynum").innerHTML = dayNumber;
+// document.querySelector(".year").innerHTML = year;
+
+
